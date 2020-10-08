ecc2f83aa1f6f25611d75849ec696c4d226e958c
Joseph cohen <jojocohen57@gmail.com>
2020-10-08 18:40:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function moveDodgerRight() {
   var leftNumbers = dodger.style.left.replace('px', '');
   var left = parseInt(leftNumbers, 10)
   if (left > 0) {
-    dodger.style.left = `${left + 1}px`
+    dodger.style.left = `${left + 10}px`
   }
 }
 
